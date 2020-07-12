7284b248ae855b20a19c1f691e05fc87fe654e95
Wellington Mascena <wellington.mascena@gmail.com>
2020-07-12 13:12:53 -0300

    Exercicio 003: programa que ler dois números e retorna a soma

---
+++ exe003.py
@@ -0,0 +1,8 @@
+# Exercício 003: Leia dos números e mostre a soma deles
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite um outro valor: '))
+
+soma = num1 + num2
+
+print('O resultado da soma de {} com {} é {}!'.format(num1, num2, soma))
