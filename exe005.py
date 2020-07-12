c124dcd21b1a43c1efc00db28472eadd17835081
Wellington Mascena <wellington.mascena@gmail.com>
2020-07-12 14:17:13 -0300

    Exercicio 005 ler número e mostrar seu antencessor e sucessor

---
+++ exe005.py
@@ -0,0 +1,3 @@
+#Exercício 005: Programa que ler um número inteiro e mostra o antencessor e o sucessor
+num = int(input('Digite um número: '))
+print('Analisando o valor {}, seu antencessor é {} e o seu sucessor é {}'.format(num, (num-1),(num+1)))
