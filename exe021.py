64c5ad3b8f7ec0c8f13900effc3f780a1d2d1aa7
Wellington Mascena <wellington.mascena@gmail.com>
2020-08-02 20:44:27 -0300

    Exercício 021: reproduzindo mp3 com pygame.

--- exe021.py
+++ exe021.py
@@ -1,6 +1,8 @@
 import pygame
+import os
+fileMp3 = os.path.join(os.path.dirname(__file__),'hero.mp3')
 pygame.mixer.init()
-pygame.mixer.music.load('hero.mp3')
+pygame.mixer.music.load(fileMp3)
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
